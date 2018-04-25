76655f178a15cf1a5e62ff9300a6217e094e94e2
Christoph Sonnberger <christoph.sonnberger@posteo.at>
2018-04-25 10:58:37 +0200

    Added mySql query

--- SEC/src/main/java/hello/WebController.java
+++ SEC/src/main/java/hello/WebController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import java.sql.*;
 
 @Controller
 public class WebController implements WebMvcConfigurer {
@@ -33,8 +33,9 @@ public class WebController implements WebMvcConfigurer {
 
     @PostMapping("/userform")
     public String getUserData(@Valid UserForm userForm, BindingResult bindingResult) {
-        if(userForm.getClient().equals("Hans")){ // Query DB
+        if(checkMail(userForm.getClient())){
             return "redirect:/results";
+        }
         }else{
             return "userform";
         }
@@ -51,5 +52,41 @@ public class WebController implements WebMvcConfigurer {
         return "redirect:/userform";
     }
 
+    public boolean checkMail(String email){
+        try
+        {
+            // create our mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://192.168.56.3";
+            Class.forName(myDriver);
+            //TODO: Passwort darf nicht als Klartext da stehen
+            Connection conn = DriverManager.getConnection(myUrl, "sec_read", "pai1hdsfa!shjASDFfdpasdhf");
+
+            // our SQL SELECT query.
+            // if you only need a few columns, specify them by name instead of using "*"
+            String query = "SELECT Email FROM User";
+
+            // create the java statement
+            Statement st = conn.createStatement();
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // iterate through the java resultset
+            while (rs.next())
+            {
+                if(email.equals(rs.getString("Email")));
+                st.close();
+                return true;
+            }
+            st.close();
+            return false;
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    }
 
 }
